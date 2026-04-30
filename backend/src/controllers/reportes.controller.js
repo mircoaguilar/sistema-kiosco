bb0c4180e3ce0223dc93d95b8a457f6fe80bad93
Mirco Aguilar <mircoaguilar02@gmail.com>
2026-04-29 21:16:33 -0300

    fix de reportes

--- backend/src/controllers/reportes.controller.js
+++ backend/src/controllers/reportes.controller.js
@@ -48,7 +48,7 @@ const reportesController = {
                 SELECT 
                     COALESCE(dv.descripcion_manual, p.nombre) AS nombre,
                     MAX(c.nombre_categoria) AS categoria,
-                    COALESCE(MAX(pr.nombre), 'Manual') AS proveedor,
+                    MAX(pr.nombre) AS proveedor,
                     SUM(dv.cantidad) AS cantidad,
                     SUM(dv.subtotal) AS total
                 FROM detalle_ventas dv
