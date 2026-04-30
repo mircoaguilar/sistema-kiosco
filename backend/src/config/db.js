3399cbdde9e5004c7daf7d63e734ed8aafeeb583
Mirco Aguilar <mircoaguilar02@gmail.com>
2026-04-29 21:20:27 -0300

    Fix de la base de datos horario

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,12 +6,13 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT, // Asegúrate de tener esta variable en Render
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
+    timezone: '-03:00', // Hora Argentina
     ssl: {
-        rejectUnauthorized: false // <--- ESTO ES LO QUE FALTA PARA AIVEN
+        rejectUnauthorized: false
     }
 });
 
