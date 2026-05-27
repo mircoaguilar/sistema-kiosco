8e6a218b91f8e37b95a9f20318ece9d44b3214a5
Mirco Aguilar <mircoaguilar02@gmail.com>
2026-05-27 17:38:33 -0300

    fix: config db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,15 +1,22 @@
-const { Pool } = require('pg');
+const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = new Pool({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
     ssl: {
         rejectUnauthorized: false
     }
 });
 
-module.exports = pool;
+pool.on('connection', (connection) => {
+    connection.query("SET time_zone = '-03:00'");
+});
+
+module.exports = pool.promise();
