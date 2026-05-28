cb9d517d88917068e172d1669a16c6f6594708e2
Mirco Aguilar <mircoaguilar02@gmail.com>
2026-05-28 17:12:06 -0300

    Config db para supabase

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,22 +1,11 @@
-const mysql = require('mysql2');
+const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
 });
 
-pool.on('connection', (connection) => {
-    connection.query("SET time_zone = '-03:00'");
-});
-
-module.exports = pool.promise();
+module.exports = pool;
