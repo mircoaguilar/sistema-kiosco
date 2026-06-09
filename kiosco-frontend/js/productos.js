0f7226fef682c2b83958a91d928c883e8fcc980f
Mirco Aguilar <mircoaguilar02@gmail.com>
2026-06-09 18:55:58 -0300

    Fix: no trae las categorias pantalla de productos

--- kiosco-frontend/js/productos.js
+++ kiosco-frontend/js/productos.js
@@ -23,22 +23,22 @@ document.getElementById('nombre-vendedor').innerText = `Vendedor: ${nombreUsuari
 
 document.addEventListener('DOMContentLoaded', async () => {
 
+    await cargarCategorias();
+    await cargarProveedores();
+    await cargarProductos(); 
+
     $('#filtro-categoria, #filtro-proveedor').select2({
         width: '100%'
     });
-    
-    await cargarCategorias();
-    await cargarProveedores();
-    await cargarProductos();
+    $('#filtro-categoria').trigger('change');
+    $('#filtro-proveedor').trigger('change');
 
     const el = document.getElementById('offcanvasCategorias');
-
     if (el) {
         offcanvasCategorias = new bootstrap.Offcanvas(el);
 
         document.getElementById('btnAbrirCategorias').onclick = () => {
             bootstrapModalProd.hide();
-
             setTimeout(() => {
                 offcanvasCategorias.show();
             }, 300);
@@ -91,8 +91,6 @@ async function cargarCategorias() {
         if (selectFiltro) {
             selectFiltro.innerHTML = '<option value="">Todas las Categorías</option>' + opts;
         }
-
-        $('#filtro-categoria').trigger('change'); 
 
         document.getElementById('lista-categorias-gestion').innerHTML = categorias.map(c => `
             <li class="list-group-item d-flex justify-content-between align-items-center py-2 small">
